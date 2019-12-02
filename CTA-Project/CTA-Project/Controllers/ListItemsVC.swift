fc8976c0ea36a75f6948d19f63185ffd56084664
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-12-02 13:55:17 -0500

    adds listCell objects and constraints

--- CTA-Project/CTA-Project/Controllers/ListItemsVC.swift
+++ CTA-Project/CTA-Project/Controllers/ListItemsVC.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ListItemsVC: UIViewController {
     
-    //MARK: Objects
+    //MARK: - Objects
+    var listTableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .white
+        table.register(ListCell.self, forCellReuseIdentifier: "listCell")
+        return table
+    }()
     
-    
-    //MARK: Setup
+    //MARK: - Setup
     private func setupListUI() {
         setupNavBar()
+        setTableViewContraints()
+        setDelegates()
     }
     private func setupNavBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.left.square"), style: .done, target: nil, action: nil)
+    }
+    private func setDelegates() {
+        listTableView.delegate = self
+        listTableView.dataSource = self
+    }
+    
+    //MARK: - Contraints
+    private func setTableViewContraints() {
+        view.addSubview(listTableView)
+        listTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            listTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            listTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            listTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            listTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
     }
     
     
@@ -29,8 +51,21 @@ class ListItemsVC: UIViewController {
         setupListUI()
 
     }
-    
-
-    
 
 }
+
+extension ListItemsVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
+}
+
---
+++ CTA-Project/CTA-Project/Views/ListCell.swift
@@ -0,0 +1,110 @@
+//
+//  ListCell.swift
+//  CTA-Project
+//
+//  Created by Angela Garrovillas on 12/2/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setImageConstraints()
+        setMainDescriptionContraints()
+        setHeartButtonContraints()
+        setAdditionalLabelConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Objects
+    var listImage: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .blue
+        var newFrame = image.frame.size
+        newFrame.width = 110
+        newFrame.height = 110
+        image.frame.size = newFrame
+        return image
+    }()
+    var mainDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Main Description"
+        return label
+    }()
+    var additionalInfo: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.text = "Additional Info"
+        return label
+    }()
+    var heartButton: UIButton = {
+        let button = UIButton()
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 40, weight: UIImage.SymbolWeight.medium)
+        let heart = UIImage(systemName: "heart", withConfiguration: config)
+        button.setImage(heart, for: .normal)
+        //heart.fill
+        
+        var newFrame = button.frame.size
+        newFrame.width = 70
+        newFrame.height = 70
+        button.frame.size = newFrame
+        
+        button.tintColor = .red
+        return button
+    }()
+    
+    //MARK: Constraints
+    func setImageConstraints() {
+        contentView.addSubview(listImage)
+        listImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            listImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            listImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            listImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            listImage.widthAnchor.constraint(equalToConstant: listImage.frame.width),
+            listImage.heightAnchor.constraint(equalToConstant: listImage.frame.height)
+            ])
+    }
+    func setHeartButtonContraints() {
+        contentView.addSubview(heartButton)
+        heartButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            heartButton.leadingAnchor.constraint(equalTo: mainDescriptionLabel.trailingAnchor,constant: 15),
+            heartButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)])
+    }
+    func setMainDescriptionContraints() {
+        contentView.addSubview(mainDescriptionLabel)
+        mainDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainDescriptionLabel.topAnchor.constraint(equalTo: listImage.topAnchor),
+            mainDescriptionLabel.leadingAnchor.constraint(equalTo: listImage.trailingAnchor, constant: 15),
+            mainDescriptionLabel.widthAnchor.constraint(equalToConstant: 175)
+        ])
+    }
+    func setAdditionalLabelConstraints() {
+        contentView.addSubview(additionalInfo)
+        additionalInfo.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            additionalInfo.leadingAnchor.constraint(equalTo: mainDescriptionLabel.leadingAnchor),
+            additionalInfo.topAnchor.constraint(equalTo: mainDescriptionLabel.bottomAnchor, constant: 20),
+            additionalInfo.trailingAnchor.constraint(equalTo: mainDescriptionLabel.trailingAnchor)])
+    }
+    
+    
+}
